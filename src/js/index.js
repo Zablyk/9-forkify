48851a36939d77b8d86c99038c34c2fc69c8b25d
Oleg Zablotskyi <Zablotsky@gmail.com>
2019-09-23 18:58:28 -0400

    add try and catch to index html and it works

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,17 @@
 import axios from 'axios';
 
 async function getResults(query){
-    //const proxy = 'https://crossorigin.me/https://google.com';
+    //const proxy = 'https://crossorigin.me/https://google.com'; works without any proxy;
     const key = '301f8869c9aab7ed62e04c75a59c2727';
-    const res = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${query}`)
-    console.log(res);
+    try{
+        const res = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${query}`)
+        const recipes = res.data.recipes;
+        console.log(recipes);
+    } catch(error){
+        alert(error);
+    } 
 }; 
-getResults();
+getResults('tomato pasta');
 
 
 //https://www.food2fork.com/api/search
