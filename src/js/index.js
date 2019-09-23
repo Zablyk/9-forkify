00f110c12e66dcdefa4f2e4493bf6ff05741e1bd
Oleg Zablotskyi <Zablotsky@gmail.com>
2019-09-23 16:49:20 -0400

    import all in index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,5 @@
 import str from './models/Search';
-import { add, multiply, ID} from './views/searchView';
+//import { add as a, multiply as m, ID} from './views/searchView';
+import * as searchView from './views/searchView';
 
-console.log(`Using imported functions ${add(ID, 2)} and ${multiply(3, 5)}. ${str}`);
+console.log(`Using imported functions ${a(ID, 2)} and ${m(3, 5)}. ${str}`);
