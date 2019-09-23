718ccfc0e91562f6e0ef0871137417fb859dc609
Oleg Zablotskyi <Zablotsky@gmail.com>
2019-09-23 19:58:08 -0400

    sent API with querypizza and got result

---
+++ src/js/index.js
@@ -0,0 +1,4 @@
+import Search from './models/Search';
+
+const search = new Search('pizza');
+console.log(search);
--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -5,12 +5,12 @@ export default class Search {
     constructor(query) {
         this.query = query;
     }
-    async getResults(query){
+    async getResults(){
         //const proxy = 'https://crossorigin.me/https://google.com'; works without any proxy;
         const key = '301f8869c9aab7ed62e04c75a59c2727';
         try{
-            const res = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${query}`)
-            const recipes = res.data.recipes;
+            const res = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${this.query}`)
+            this.result = res.data.recipes;
             console.log(recipes);
         } catch(error){
             alert(error);
