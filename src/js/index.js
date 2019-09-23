515c26969d516c17513f5a21a874497fc3730818
Oleg Zablotskyi <Zablotsky@gmail.com>
2019-09-23 16:57:59 -0400

    clear search.ja, index.js and searchView.js

--- src/js/index.js
+++
@@ -1,5 +0,0 @@
-import str from './models/Search';
-//import { add as a, multiply as m, ID} from './views/searchView';
-import * as searchView from './views/searchView';
-
-console.log(`Using imported functions ${a(ID, 2)} and ${m(3, 5)}. ${str}`);
