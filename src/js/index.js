73b418ace483c40dabcdd0b7359e7e8d3d24cfcf
Oleg Zablotskyi <Zablotsky@gmail.com>
2019-09-23 20:22:51 -0400

    add state object and search event listener

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,27 @@
 import Search from './models/Search';
 
+/**Global state of the app
+ * - Search object
+ * - Current recipe object
+ * - Shopping list object
+ * - Liked recipes  
+ */
+const = state{};
+
+const controlSearch = () => {
+    //Get a query from a view
+    const query = 'pizza';//TODO
+
+    if(query) {
+        //New search object and add to state
+    }
+};
+
+document.querySelector('.search').addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+});
+
 const search = new Search('pizza');
 console.log(search);
+search.getResults();    
--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -11,7 +11,7 @@ export default class Search {
         try{
             const res = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${this.query}`)
             this.result = res.data.recipes;
-            console.log(recipes);
+            //console.log(this.result);
         } catch(error){
             alert(error);
         } 
