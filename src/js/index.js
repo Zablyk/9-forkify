44644fdc3bace1aecfdae0216982beb64bb0e514
Oleg Zablotskyi <Zablotsky@gmail.com>
2019-09-24 15:31:29 -0400

    i have a input value from a search field

--- src/js/index.js
+++ src/js/index.js
@@ -12,7 +12,8 @@ const state = {};
 
 const controlSearch = async () => {
     // 1) Get a query from a view
-    const query = 'pizza';//TODO
+    const query = searchView.getInput();
+    console.log(query);
 
     if(query) {
         // 2) New search object and add to state
