df9111c0b3a23d7b16954cb3c6ca44bfc9af7c52
Oleg Zablotskyi <Zablotsky@gmail.com>
2019-09-24 20:15:02 -0400

    add function             <h4 class=results__name>${limitRecipeTitle(recipe.title)}</h4>

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -8,7 +8,29 @@ export const clearInput = () =>  {
 
 export const clearResults = () => {
     elements.searchResList.innerHTML = '';
-}
+};
+
+/*
+//example: "Pasta with tomato and spinach"
+acc: 0 / acc + cur.length = 5 / newTitle = ['Pasta']
+acc: 5 / acc + cur.length = 9 / newTitle = ['Pasta', 'with']
+acc: 9 / acc + cur.length = 15 / newTitle = ['Pasta', 'with', 'tomato']
+acc: 15 / acc + cur.length = 18 / newTitle = ['Pasta', 'with', 'tomato']
+acc: 18 / acc + cur.length = 25 / newTitle = ['Pasta', 'with', 'tomato']
+*/
+const limitRecipeTitle = (title, limit = 17) => {
+    const newTitle = [];
+    if (title.length > limit) {
+       title.split(' ').reduce((acc, cur) => {
+        if (acc + cur.length <= limit){
+            newTitle.push(cur);
+        }
+        return acc + cur.length
+       }, 0); 
+       return (`${newTitle.join(' ')}...`);
+    } 
+    return title;
+} 
 
 const renderRecipe = recipe => {
     const markup = `
@@ -18,7 +40,7 @@ const renderRecipe = recipe => {
             <img src="${recipe.image_url}" alt=${recipe.title}>
         </figure>
         <div class="results__data">
-            <h4 class="results__name">${recipe.title}</h4>
+            <h4 class="results__name">${limitRecipeTitle(recipe.title)}</h4>
             <p class="results__author">${recipe.publisher}</p>
         </div>
     </a>
