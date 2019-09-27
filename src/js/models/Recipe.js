41fb78365f9006469a10d685a885644cd18c6942
Oleg Zablotskyi <zablotsky@gmail.com>
2019-09-27 13:13:03 -0400

    creating calTime and calsServing

--- src/js/models/Recipe.js
+++ src/js/models/Recipe.js
@@ -10,16 +10,28 @@ export default class Recipe {
 
     async getRecipe() {
         try { 
-            const res = await axios (`https://www.food2fork.com/api/get?key=${key}&rID=${this.id}`);
+            const res = await axios (`https://www.food2fork.com/api/get?key=${key}&rId=${this.id}`);
             this.title = res.data.recipe.title;
             this.author = res.data.recipe.publisher;
-            this.image = res.data.recipe.img_url; 
+            this.image = res.data.recipe.image_url; 
             this.url = res.data.recipe.source_url;
             this.ingredients = res.data.recipe.ingredients;
             console.log(res);
         } catch (error) {
             console.log(error);
+            alert('Something went wrong :(');
         }
+
+        // // // calcTime() {
+        // // //     //We have a 3 ingredients and we need a 15 minutes per each ing
+        // // //     const numIng = this.ingredients.length;
+        // // //     const periods = Math.ceil(numIng / 3);
+        // // //     this.time = periods * 15; 
+        // // // }
+
+        // // // calcServings() {
+        // // //     this.servings = 4;
+        // // // }
     }
     
 }
