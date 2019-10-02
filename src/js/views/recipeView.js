2c3b488caf9596b3e291e085c47eabbd7aa52654
Oleg Zablotskyi <Zablotsky@gmail.com>
2019-10-02 17:12:53 -0400

    added function numberToFraction and it works

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,15 +1,46 @@
 import { elements } from './base';
-import { Fraction } from 'fractional';
+//import { Fraction } from 'fractional';
 
 export const clearRecipe = () => {
        elements.recipe.innerHTML = '';
  }
 
+ var numberToFraction = function( amount ) {
+	// This is a whole number and doesn't need modification.
+	if ( parseFloat( amount ) === parseInt( amount ) ) {
+		return amount;
+	}
+	// Next 12 lines are cribbed from https://stackoverflow.com/a/23575406.
+	var gcd = function(a, b) {
+		if (b < 0.0000001) {
+			return a;
+		}
+		return gcd(b, Math.floor(a % b));
+	};
+	var len = amount.toString().length - 2;
+	var denominator = Math.pow(10, len);
+	var numerator = amount * denominator;
+	var divisor = gcd(numerator, denominator);
+	numerator /= divisor;
+	denominator /= divisor;
+	var base = 0;
+	// In a scenario like 3/2, convert to 1 1/2
+	// by pulling out the base number and reducing the numerator.
+	if ( numerator > denominator ) {
+		base = Math.floor( numerator / denominator );
+		numerator -= base * denominator;
+	}
+	amount = Math.floor(numerator) + '/' + Math.floor(denominator);
+	if ( base ) {
+		amount = base + ' ' + amount;
+	}
+	return amount;
+};
 
-const formatCount = count => {
+/*const formatCount = count => {
     if (count) {
-        //Ex. count = 2.5 --> 2 1/2
-        //Ex. count = 0.5 --> 1/2
+        Ex. count = 2.5 --> 2 1/2
+        Ex. count = 0.5 --> 1/2
         const [int, dec] = count.toString().split('.').map(el => parseInt(el, 10));
         
         if(!dec) return count;
@@ -25,13 +56,15 @@ const formatCount = count => {
     }
     return '?';
 };
+*/
+
 
 const createIngredient = ingredient => `
     <li class="recipe__item">
         <svg class="recipe__icon">
             <use href="img/icons.svg#icon-check"></use>
         </svg>
-        <div class="recipe__count">${formatCount(ingredient.count)}</div>
+        <div class="recipe__count">${numberToFraction(ingredient.count)}</div>
         <div class="recipe__ingredient">
             <span class="recipe__unit">${ingredient.unit}</span>
             ${ingredient.ingredient}
