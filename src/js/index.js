949e8de4c5bdd5ff8d3e87084fecca11845821dd
Oleg Zablotskyi <Zablotsky@gmail.com>
2019-10-03 16:16:05 -0400

    added methods to dlete and update count into our List

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,6 @@
 import Search from './models/Search';
 import Recipe from './models/Recipe';
+import List from './models/List';
 import * as searchView from './views/searchView'; 
 import * as recipeView from './views/recipeView'; 
 import { elements, renderLoader, clearLoader } from './views/base';
@@ -84,7 +85,6 @@ const controlRecipe = async () => {
        try{
         //Get recipe data and parse ingredients
         await state.recipe.getRecipe();
-        console.log(state.recipe.ingredients);
         state.recipe.parseIngredients();
 
         //Calculate servings and coocing time
@@ -119,5 +119,7 @@ elements.recipe.addEventListener('click', e => {
         state.recipe.updateServings('inc');
         recipeView.updateServingsInredients(state.recipe); 
     }
-    console.log(state.recipe);
 });
+
+window.l = new List();
+
