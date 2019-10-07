680a8d7b73ecc1881645716d9750d03b10c312c3
Oleg Zablotskyi <Zablotsky@gmail.com>
2019-10-07 16:00:38 -0400

    adding shopping list

--- src/js/views/listView.js
+++ src/js/views/listView.js
@@ -1,2 +1,22 @@
 import { elements } from './base';
 
+export const renderItem = (item => {
+    const markup = `
+            <li class="shopping__item">
+                <div class="shopping__count">
+                    <input type="number" value="500" step="100">
+                    <p>g</p>
+                </div>
+                <p class="shopping__description">Pasta</p>
+                <button class="shopping__delete btn-tiny">
+                    <svg>
+                        <use href="img/icons.svg#icon-circle-with-cross"></use>
+                    </svg>
+            </button>
+            </li>   
+    `;
+});
+
+export const deleteItem = (id => {
+
+});
